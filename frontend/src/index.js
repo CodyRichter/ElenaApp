1f873df376a0e585ee2c915312ff19e21787e680
sohanshow <sohanshow@gmail.com>
2022-11-07 18:57:28 -0500

    + resolved error of multiple import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
 import ReactDOM from "react-dom";
-import './style/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import "./style/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
