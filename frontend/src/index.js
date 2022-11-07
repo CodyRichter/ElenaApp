485ab9aa06359f69d8b8b592d75aefa4e60f37bf
sohanshow <sohanshow@gmail.com>
2022-11-07 17:34:38 -0500

    + map implementation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
 import './style/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
