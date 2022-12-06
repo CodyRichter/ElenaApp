4fafbb243b3aaa172ff7681adebfb4f26e092303
sohanshow <sohanshow@gmail.com>
2022-12-06 14:36:03 -0500

    + Added token for API and formatting changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,41 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Authentication from './pages/Authentication/Authentication';
-import Navigation from './pages/Navigation/Navigation';
-import isEmpty from 'lodash/isEmpty';
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Authentication from "./pages/Authentication/Authentication";
+import Navigation from "./pages/Navigation/Navigation";
+import isEmpty from "lodash/isEmpty";
 
 function App() {
 
     const [token, setToken] = React.useState<string | null>(null);
 
     useEffect(() => {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem("token");
         if (!isEmpty(token)) {
-            setToken(token);
+          setToken(token);
         }
-    }, []);
+      }, []);
 
-    async function updateAndSaveToken(token: string) {
-        localStorage.setItem('token', token);
-        setToken(token);
-    }
+  async function updateAndSaveToken(token: string) {
+    localStorage.setItem("token", token);
+    setToken(token);
+  }
 
-    return (
-        <BrowserRouter>
-            {isEmpty(token) ? (
-                <Authentication setToken={updateAndSaveToken} />
-            ) : (
-
-                <Routes>
-                    <Route path="/navigate" element={<Navigation />} />
-                    <Route path="/login" element={<Authentication setToken={updateAndSaveToken} />} />
-                    <Route path="/" element={<Navigation />} />
-                </Routes>
-            )
-            }
-        </BrowserRouter >
-    );
+  return (
+    <BrowserRouter>
+      {isEmpty(token) ? (
+        <Authentication setToken={updateAndSaveToken} />
+      ) : (
+        <Routes>
+          <Route path="/navigate" element={<Navigation />} />
+          <Route
+            path="/login"
+            element={<Authentication setToken={updateAndSaveToken} />}
+          />
+          <Route path="/" element={<Navigation />} />
+        </Routes>
+      )}
+    </BrowserRouter>
+  );
 }
 
 export default App;
