5d74e872c42a203b4c6f094e5d2f719c6b292455
Casey Ryan <caseyryan22465@gmail.com>
2022-12-07 15:59:36 -0500

    did custom A* so i have more control, and added strategies + tests for min and max elevation
    min and max elevation likely do not work. Do not know how to find a ground truth for this.
    max_dist functionality not impl yet, but shouldnt be too hard (just calculate direct first)

--- backend/src/modules/pathfinding/a_star.py
+++ backend/src/modules/pathfinding/a_star.py
@@ -1,9 +1,12 @@
 import abc
+import heapq
+import math
 from typing import List, Tuple
 
 import networkx as nx
 import osmnx as ox
 from haversine import haversine
+from src.modules.pathfinding.osm_util import *
 
 try:
     graph = ox.load_graphml("Amherst.graphml")
@@ -24,32 +27,57 @@ class AStar:
         self._routing_strategy = routing_strategy
     
     def get_route(self, origin: Tuple[float, float], dest: Tuple[float, float]) -> Tuple[List[Tuple[float, float]], float]:
-        start = ox.nearest_nodes(graph, origin[1], origin[0])
-        end = ox.nearest_nodes(graph, dest[1], dest[0])
-
-        shortest_path_ids = nx.astar_path(
-            self._routing_strategy.graph,
-            start,
-            end,
-            self._routing_strategy.heuristic,
-            self._routing_strategy.weight
-            )
-        shortest_path_coords = list(map(
-            lambda id: (
-                self._routing_strategy.graph.nodes[id]['y'],
-                self._routing_strategy.graph.nodes[id]['x']
-                ),shortest_path_ids))
+        start = ox.nearest_nodes(self._routing_strategy.graph, origin[1], origin[0])
+        end = ox.nearest_nodes(self._routing_strategy.graph, dest[1], dest[0])
         
-        prev = shortest_path_coords[0]
-        dist = 0
-        for pt in shortest_path_coords:
-            dist += haversine(prev, pt)
-            prev = pt
-        return shortest_path_coords, dist
+        shortest_path_ids = []
+        ## Begin A* stuff
+        adj = self._routing_strategy.graph.adj
+        queue = [(0, start)]
+        costs = {start: 0}
+        came_from = {start: None}
+        
+
+        while queue:
+            _, cur = heapq.heappop(queue)
+
+            if cur == end:
+                # Reconstruct path
+                shortest_path_ids = [cur]
+                node = came_from[cur]
+                while node is not None:
+                    shortest_path_ids.append(node)
+                    node = came_from[node]
+                shortest_path_ids.reverse()
+
+                # form return into function definition
+                shortest_path_coords = list(map(
+                    lambda id: (
+                        self._routing_strategy.graph.nodes[id]['y'],
+                        self._routing_strategy.graph.nodes[id]['x']
+                        ),shortest_path_ids))
+                gain = 0
+                # Debug stuff for elevation testing
+                #prev = shortest_path_ids[0]
+                #for pt in shortest_path_ids:
+                #    if 'elevation' in self._routing_strategy.graph.nodes[pt]:
+                #        gain += max(self._routing_strategy.graph.nodes[pt]['elevation'] - self._routing_strategy.graph.nodes[prev]['elevation'], 0)
+                #print("total gain:", gain)
+                return shortest_path_coords, costs[end]
+            
+            for neighbor in adj[cur]:
+                neighbor_cost = costs[cur] + self._routing_strategy.weight(cur, neighbor, None)
+                if neighbor not in costs or neighbor_cost < costs[neighbor]:
+                    came_from[neighbor] = cur
+                    costs[neighbor] = neighbor_cost
+                    fscore = neighbor_cost + self._routing_strategy.heuristic(neighbor, end)
+                    heapq.heappush(queue, (fscore, neighbor))
+
+        raise RuntimeError("NO PATH")
 
 
 class RoutingStrategy(abc.ABC):
-    def __init__(self, graph, max_dist):
+    def __init__(self, graph: networkx.MultiDiGraph, max_dist):
         self.graph = graph
         self.max_dist = max_dist
 
@@ -76,13 +104,68 @@ class DirectStrategy(RoutingStrategy):
         n2 = (self.graph.nodes[node2]['y'], self.graph.nodes[node2]['x'])
         return haversine(n1, n2)
 
+class MinimizeStrategy(RoutingStrategy):
+    def __init__(self, graph, max_dist):
+        super().__init__(graph, max_dist)
+        add_elevation(graph)
+    
+    def heuristic(self, node, target):
+        n1 = (self.graph.nodes[node]['y'], self.graph.nodes[node]['x'])
+        n2 = (self.graph.nodes[target]['y'], self.graph.nodes[target]['x'])
+        hav = haversine(n1, n2)
+        
+        e1 = self.graph.nodes[node]['elevation']
+        e2 = self.graph.nodes[target]['elevation']
+        if(e1 < e2):
+            # we have to go uphill at some point, penalize
+            #return math.sqrt(hav**2 + (e2-e1)**2)#distance including elevation
+            hav += 10*(e2 - e1)
+        return hav # dont reward for downhill
+    
+    def weight(self, node1, node2, edge_attrs):
+        n1 = (self.graph.nodes[node1]['y'], self.graph.nodes[node1]['x'])
+        n2 = (self.graph.nodes[node2]['y'], self.graph.nodes[node2]['x'])
+        hav = haversine(n1, n2)
+        
+        e1 = self.graph.nodes[node1]['elevation']
+        e2 = self.graph.nodes[node2]['elevation']
+        if(e1 < e2):
+            #return math.sqrt(hav**2 + (e2-e1)**2)#distance including elevation
+            hav += 10*(e2 - e1)
+        return hav # dont reward for downhill
+
+class MaximizeStrategy(RoutingStrategy):
+    def __init__(self, graph, max_dist):
+        super().__init__(graph, max_dist)
+        add_elevation(graph)
+    
+    def heuristic(self, node, target):
+        n1 = (self.graph.nodes[node]['y'], self.graph.nodes[node]['x'])
+        n2 = (self.graph.nodes[target]['y'], self.graph.nodes[target]['x'])
+        hav = haversine(n1, n2)
+        
+        e1 = self.graph.nodes[node]['elevation']
+        e2 = self.graph.nodes[target]['elevation']
+        #if(e2 < e1):
+        #    return math.sqrt(hav**2 + (e1-e2)**2)#distance including elevation drop1
+        return math.sqrt(hav**2 + (max(e1-e2, 0))**2) #max because if downhill, then it will lose negative and thus reward
+    
+    def weight(self, node1, node2, edge_attrs):
+        n1 = (self.graph.nodes[node1]['y'], self.graph.nodes[node1]['x'])
+        n2 = (self.graph.nodes[node2]['y'], self.graph.nodes[node2]['x'])
+        hav = haversine(n1, n2)
+        
+        e1 = self.graph.nodes[node1]['elevation']
+        e2 = self.graph.nodes[node2]['elevation']
+        return math.sqrt(hav**2 + (max(e1-e2, 0))**2) #max because if downhill, then it will lose negative and thus reward
+
 def getStrategy(mode: str, max_dist: int) -> RoutingStrategy:
     if mode == "direct":
         return DirectStrategy(graph, max_dist)
     elif mode == "maximize_elevation":
-        raise NotImplementedError
+        return MaximizeStrategy(graph, max_dist)
     elif mode == "minimize_elevation":
-        raise NotImplementedError
+        return MinimizeStrategy(graph, max_dist)
     else:
         raise RuntimeError(f"Invalid mode: {mode}")
 
--- backend/src/test/test_nav.py
+++ backend/src/test/test_nav.py
@@ -10,8 +10,8 @@ client = TestClient(app)
 @pytest.mark.timeout(10)
 def test_nav_direct_route_named():
     req = NavigationRequest(
-        origin = "Amherst Cinema",
-        destination = "Beneski Museum of Natural History",
+        origin = "Fine Arts Center, Amherst",
+        destination = "Pokeberry Ridge, Amherst",
         mode = "direct",
         max_distance = 10
     )
@@ -21,12 +21,54 @@ def test_nav_direct_route_named():
     assert len(json["waypoints"]) > 2 #path must be more than just start->finish
 
     prev = json["waypoints"][0]
-    #starting coordinate is close to requested origin
-    assert haversine(req.origin, prev) < 0.1
     acc = 0
     for pt in json["waypoints"]:
         acc += haversine(pt, prev)
         prev = pt
-    #ending coordinate is close to requested destination
-    assert haversine(req.destination, prev) < 0.1
+        print(pt)
     assert acc >= haversine(json["waypoints"][0], json["waypoints"][-1])#lower bounded by straight line
+
+@pytest.mark.timeout(20)
+def test_nav_min_route_named():
+    req = NavigationRequest(
+        origin = "Fine Arts Center, Amherst",
+        destination = "Pokeberry Ridge, Amherst",
+        mode = "minimize_elevation",
+        max_distance = 10
+    )
+    response = client.post("/nav/path", json=req.dict())
+    assert response.status_code == 200
+    json = response.json()
+    assert len(json["waypoints"]) > 2 #path must be more than just start->finish
+
+    prev = json["waypoints"][0]
+    acc = 0
+    for pt in json["waypoints"]:
+        acc += haversine(pt, prev)
+        prev = pt
+    assert acc >= haversine(json["waypoints"][0], json["waypoints"][-1])#lower bounded by straight line)
+    #print(acc)
+    assert acc > 0.997 #hardcoded FAC->Pokeberry, min elev route should do some roundabout way of getting there
+
+@pytest.mark.timeout(20)
+def test_nav_max_route_named():
+    req = NavigationRequest(
+        origin = "Fine Arts Center, Amherst",
+        destination = "Pokeberry Ridge, Amherst",
+        mode = "maximize_elevation",
+        max_distance = 10
+    )
+    response = client.post("/nav/path", json=req.dict())
+    assert response.status_code == 200
+    json = response.json()
+    assert len(json["waypoints"]) > 2 #path must be more than just start->finish
+
+    prev = json["waypoints"][0]
+    acc = 0
+    for pt in json["waypoints"]:
+        acc += haversine(pt, prev)
+        prev = pt
+    assert acc >= haversine(json["waypoints"][0], json["waypoints"][-1])#lower bounded by straight line)
+    #print(acc)
+    assert acc > 0.997 #hardcoded FAC->Pokeberry, max elev route should do some roundabout way of getting there
+    assert abs(acc - 1.59) > 0.1 #hardcoded res of min route. min route could be wrong too, but max and min routes should not be the same
